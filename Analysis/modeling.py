69bc31f7de2c1ccfdaedebd8ebb527bc8f506635
Ben Southgate <bsouthga@gmail.com>
2013-10-22 22:17:08 -0400

    Add import data

--- Analysis/modeling.py
+++ Analysis/modeling.py
@@ -5,10 +5,17 @@
 import shelve
 import pandas as pd
 
+# Pull data from shelf
+def import_data():
+    storage = shelve.open(os.path.join(path,'Pitchfork_Data_Shelf'))
+    data = storage['data frame']
+    storage.close()
+    return data
 
 
-
-
+def main():
+    
+    data_frame = import_data()
 
 
 if __name__ == "__main__":
